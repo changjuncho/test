347171f7a7c22e2d70fdc556a4269fb2b1b06cf1
changjun <changjun.cho@gmail.com>
2020-04-21 21:56:00 +0900

    공부하자

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("반갑습니다.");
+        System.out.println("창준이 파이팅!");
     }
 }
